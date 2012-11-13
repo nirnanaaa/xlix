7a03f42705e41160c87e2771edf1c6c391faf916
fkasper <flo@khnetworks.com>
2012-11-13 17:34:41 +0100

    added several controller extensions

---
+++ xlix/lib/Xlix/Bundle/Controller/Response.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Xlix package.
+ *
+ * (c) Florian Kasper <xlix@khnetworks.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Xlix\Bundle\Controller;
+
+class Response extends ControllerOverride{
+
+    public function __construct() {
+        
+    }
+
+}
+
+?>
