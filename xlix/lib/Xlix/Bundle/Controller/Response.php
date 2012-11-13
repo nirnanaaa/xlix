74cb313d46b4fd69a0c1d5c5f9bd4ce4f8f8ac55
TheMosny <flo@khnetworks.com>
2012-11-13 18:44:01 +0100

    created the first file of my #response class for #xlix

--- xlix/lib/Xlix/Bundle/Controller/Response.php
+++ xlix/lib/Xlix/Bundle/Controller/Response.php
@@ -11,9 +11,41 @@
 
 namespace Xlix\Bundle\Controller;
 
-class Response extends ControllerOverride{
+use Xlix\Bundle\Response\Headers\Headers;
+use Xlix\Bundle\Response\Get\Get;
+use Xlix\Bundle\Response\Put\Put;
+use Xlix\Bundle\Response\File\File;
+use Xlix\Bundle\Response\Post\Post;
+use Xlix\Bundle\Response\Server\Server;
 
-    public function __construct() {
+class Response {
+
+    public $headers;
+    public $get;
+    public $post;
+    public $put;
+    public $file;
+    public $server;
+    private $data;
+    private $status;
+
+    public function __construct($data = null, $status = null) {
+        $this->headers = new Headers();
+        $this->get = new Get();
+        $this->put = new Put();
+        $this->post = new Post();
+        $this->file = new File();
+        $this->server = new Server();
+        if (!is_null($data)) {
+            $this->data = $data;
+        }
+        if (!is_null($status)) {
+            $this->status = $status;
+        }
+        $this->spinUp();
+    }
+
+    public function spinUp() {
         
     }
 
